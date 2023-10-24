a048cbe775fcfe3ab0d8c06dc74935e493e07aaa
zvezdinv <atgroup09@yandex.ru>
2023-10-24 17:48:26 +0500

    - removed README.odg, .pdf
    main.py
    + comments

--- rpico-led/main.py
+++ rpico-led/main.py
@@ -1,18 +1,20 @@
-# Библиотека для работы с пинами ввода-вывода
-from machine import Pin
-# Библиотека для работы с временем
-import time
- 
-# Светодиод в режим выхода на 25 пине
-led = Pin(25, Pin.OUT)
- 
-while True:
-    # Зажигаем светодиод
-    led.value(1)
-    # Ждём 1 секунду
-    time.sleep(1)
-    # Гасим светодиод
-    led.value(0)
-    # Ждём 1 секунду
-    time.sleep(1)
+# @page Main cycle
+#       RPico 2020, micro-Python SDK
+#       led-blink example
+# @date 2023.08.09
 
+from machine import Pin
+import time
+
+
+# CORE0 PROCESSES
+ 
+# Init. Periphery
+led = Pin(25, Pin.OUT)
+
+# Main cycle
+while True:
+    led.value(1)
+    time.sleep(1)
+    led.value(0)
+    time.sleep(1)
